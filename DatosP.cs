e1afd93f28ed0fc368a03f1e0caadcc3cdbdd0c9
VictorGMO <vm.garofaloo@uea.edu.ec>
2025-12-13 21:39:25 -0500

    Tarea Semana 3: Gestión de estudiante con arrays

---
+++ DatosP.cs
@@ -0,0 +1,17 @@
+namespace Semana3
+
+{
+    public class DatosP
+    {
+    private int id= 12345;
+    private string name= "Juan Pablo"; 
+    private string lastName= "Sanchez Guzman";
+    private string Direccion= "Av. Amazonas N34-56 y Av. Colón";
+    public void datos()
+        {
+        Console.WriteLine("ID:"+id);
+        Console.WriteLine("Nombre:"+name);
+        Console.WriteLine("Apellido:"+lastName);
+        Console.WriteLine("Direccion:"+Direccion);
+        }
+}}
