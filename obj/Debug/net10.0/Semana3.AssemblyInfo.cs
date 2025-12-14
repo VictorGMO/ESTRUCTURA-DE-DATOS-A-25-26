174405659dc3361550a1b4f6da455bcf85ce8c7e
VictorGMO <vm.garofaloo@uea.edu.ec>
2025-12-13 21:45:50 -0500

    Tarea Semana 3: Registro de estudiante con array de teléfonos

--- obj/Debug/net10.0/Semana3.AssemblyInfo.cs
+++ obj/Debug/net10.0/Semana3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semana3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1afd93f28ed0fc368a03f1e0caadcc3cdbdd0c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semana3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semana3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
